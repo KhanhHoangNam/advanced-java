ad2304090b249f9245fb6d6f147d7bd00d55d826
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-12-16 15:19:40 +0700

    linear recursion

--- src/comparable/Main.java
+++ src/comparable/Main.java
@@ -5,7 +5,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
+    private static void h10ToH2(int n) {
+        if (n > 0) {
+            int sd = n % 2;
+            n = n / 2;
+            h10ToH2(n);
+            System.out.print(sd);
+        }
+    }
     public static void main(String[] args) {
+        int n = 100;
+        h10ToH2(n);
         List<Person> people = new ArrayList<>();
         people.add(new Person(1, "KhanhHN"));
         people.add(new Person(5, "AnhPT"));
