29f2c962128e177bb60fdbeced92f317802ca5b5
KhanhHoangNam <chienbinhbattu9xpro@gmail.com>
2019-12-20 17:17:07 +0700

    multithreading in java

---
+++ src/stack_and_heap/Main.java
@@ -0,0 +1,10 @@
+package stack_and_heap;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person;
+        person = new Person("KhanhHN");
+        System.out.println(person);
+        System.out.println(person.getName());
+    }
+}
